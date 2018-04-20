8d86f9d4ab6bdbeb89a4906183a4ba8251459982
Marc Giersch <marc.giersch@innoq.com>
2018-04-20 10:41:57 +0200

    simple validation function

---
+++ validate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function validate () {
+    count=$(ls -t data/blocks/ | head -c1)
+
+    for ((i=1;i<=$count;i++)); do
+        sum=$(sha256sum ./data/blocks/$i | head -c64)
+        check=$(echo data/blocks/$(expr $i + 1) | ./JSON.awk | grep -i previousBlockHash | cut -f2 | sed s/\"//g)
+        if [[ "$sum" == "$check" ]]; then
+            echo -e "block $i ok"
+        else
+            echo -e "block $i NOT ok"
+            return 1
+        fi
+    done
+    return 0
+}
